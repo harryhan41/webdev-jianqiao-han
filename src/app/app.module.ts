e6945e49ba931f14e86216c9728c3583c26c56da
jianqiaohan <han.jian@husky.neu.edu>
2019-02-17 16:37:40 -0800

    1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import {routing} from './app.routing';
 
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './views/user/login/login.component';
-import { ProfileComponent } from './views/user/profile/profile.component';
-import { RegisterComponent } from './views/user/register/register.component';
-import { WebsiteListComponent } from './views/website/website-list/website-list.component';
-import { WebsiteNewComponent } from './views/website/website-new/website-new.component';
-import { WebsiteEditComponent } from './views/website/website-edit/website-edit.component';
-import { PageListComponent } from './views/page/page-list/page-list.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { WebsiteListComponent } from './components/website/website-list/website-list.component';
+import { WebsiteNewComponent } from './components/website/website-new/website-new.component';
+import { WebsiteEditComponent } from './components/website/website-edit/website-edit.component';
+import { PageListComponent } from './components/page/page-list/page-list.component';
+import { PageNewComponent } from './components/page/page-new/page-new.component';
+import { PageEditComponent } from './components/page/page-edit/page-edit.component';
+import { WidgetChooserComponent } from './components/widget/widget-chooser/widget-chooser.component';
+import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit.component';
+import { WidgetListComponent } from './components/widget/widget-list/widget-list.component';
+import { WidgetHeaderComponent } from './components/widget-edit/widget-header/widget-header.component';
+import { WidgetImageComponent } from './components/widget-edit/widget-image/widget-image.component';
+import { WidgetYoutubeComponent } from './components/widget-edit/widget-youtube/widget-youtube.component';
 
 
 @NgModule({
@@ -24,7 +32,15 @@ import { PageListComponent } from './views/page/page-list/page-list.component';
     WebsiteListComponent,
     WebsiteNewComponent,
     WebsiteEditComponent,
-    PageListComponent
+    PageListComponent,
+    PageNewComponent,
+    PageEditComponent,
+    WidgetChooserComponent,
+    WidgetEditComponent,
+    WidgetListComponent,
+    WidgetHeaderComponent,
+    WidgetImageComponent,
+    WidgetYoutubeComponent
   ],
   imports: [
     BrowserModule,
