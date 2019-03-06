4047926cd5d3ea0c61b0b4ef92a31907a16d39a9
jianqiaohan <han.jian@husky.neu.edu>
2019-03-06 01:05:00 -0800

    a4.6

--- src/app/views/user/register/register.component.ts
+++ src/app/views/user/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../../services/user.service.client';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('f') loginForm: NgForm;
+
+  constructor(private userService: UserService, private router: Router) {
+  }
+
 
   ngOnInit() {
   }
