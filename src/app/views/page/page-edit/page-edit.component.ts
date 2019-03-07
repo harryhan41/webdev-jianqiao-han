f69cbf6c1530852a3ee217c560c30ef45aeaf1e4
jianqiaohan <han.jian@husky.neu.edu>
2019-03-06 21:58:44 -0800

    a4.9

--- src/app/views/page/page-edit/page-edit.component.ts
+++ src/app/views/page/page-edit/page-edit.component.ts
@@ -10,19 +10,31 @@ import {PageService} from '../../../services/page.service.client';
 })
 export class PageEditComponent implements OnInit {
 
+  page_id: string;
+  web_id: string;
   page: Page;
+  pages = [{}];
 
-  constructor(private pageService: PageService, private router: ActivatedRoute) {
+  constructor(private pageService: PageService, private activeRouter: ActivatedRoute) {
+    this.page = new Page(112, 'abc', 283, 'what');
   }
 
   ngOnInit() {
-    this.router.params.subscribe(
+    this.activeRouter.params.subscribe(
       (params: any) => {
-        this.page._id = params[':pid'];
+        this.page_id = params[':pid'];
+        this.web_id = params[':wid'];
         console.log('page id: ' + this.page._id);
       },
     );
-    this.page = this.pageService.findPageById(this.page._id);
-  }
+    this.pageService.findPageById(this.page_id)
+      .subscribe(page => {
+        this.page = page;
+      });
 
+    this.pageService.findPageByWebsiteId(this.web_id)
+      .subscribe(pages => {
+        this.pages = pages;
+      });
+  }
 }
