f692cbd196d75e52e22fa1c5a8c0d199b19081ab
jianqiaohan <han.jian@husky.neu.edu>
2019-04-09 14:50:22 -0700

    redeploy

--- server.js
+++ server.js
@@ -50,4 +50,5 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+//require
 require("./assignment/app")(app);
